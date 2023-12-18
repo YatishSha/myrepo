c40c4b5029cae6ca7a65670b6238af3ce7161296
YatishSha <Yatishsharma2407@gmail.com>
2023-12-18 14:57:10 +0530

    New java file is added

---
+++ a.java
@@ -0,0 +1,18 @@
+class school
+{
+ abcd;
+
+void bublesort()
+{}
+
+void squicksort()
+{
+  
+}
+
+public static void main(String args[])
+{
+ System.out.println("hello");
+ System.out.println("hello 00000");
+}
+}
